948fd6f9dadb6a0c279d6f02a46528affffddf03
yupeng yang <54490612+carrotmax@users.noreply.github.com>
2021-09-17 02:18:35 -0400

    Update intnumber.java

--- intnumber.java
+++ intnumber.java
@@ -2,12 +2,12 @@ public class intnumber {
     
     public static void main(String[] args){
          int x =5 ;
-         if (x < 10 ){
+         if (x < 10 )
               x = x +10;
-            }
-         if (x < 10){
+            
+         if (x < 10)
               x = x + 10;
-         }
+         
         System.out.println(x);
 
 
