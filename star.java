da878a35ff076edb61f23930b88309e83b7b25de
yupeng yang <54490612+carrotmax@users.noreply.github.com>
2021-09-17 13:47:29 -0400

    star

---
+++ star.java
@@ -0,0 +1,22 @@
+public class star {
+    public static void main(String[] args){
+        
+        int row = 1;
+        int size = 5;
+        while (row <= size){
+              int col = 0;
+              while (col <  row){
+                  System.out.print('*');
+                  col = col + 1;
+              }
+              System.out.println();
+              row = row + 1;
+
+
+
+
+        }
+
+
+    }
+}
